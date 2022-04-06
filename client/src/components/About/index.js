4082d3398cf21ce89e9fc9cd629f22b6754eb8fa
Era <amirb2467@gmail.com>
2022-04-06 14:06:18 -0700

    created portfolio component with 6 projects

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,7 +6,7 @@ function About() {
             <h1 className="text-center border-bottom border-dark p-3">About Me</h1>
             <div className="text-center p-2">
                 <img src={require('../../assets/profile-pic.jpg')} className='img-thumbnail rounded'></img>
-                <p>
+                <p className="m-3">
                     I'm a current Web Developer Student looking to learn and perfect skills that can propel myself into a position to take on any project no matter the scale. From small start-ups to large corporations I aim to be able to create applications that enable future employers to reach their goals.
                     <br></br>
                     <br></br>
--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,9 +1,66 @@
 import React from "react";
 
 function Portfolio() {
+    const projects = [
+        {
+            title: 'Su Casa',
+            liveLink: 'https://mighty-taiga-27684.herokuapp.com/',
+            repoLink: 'https://github.com/amirb97/Sucasa-React-SPA-Project',
+            image: require('../../assets/sucasa.jpg')
+        },
+        {
+            title: 'Voto',
+            liveLink: 'https://dry-shelf-93414.herokuapp.com/',
+            repoLink: 'https://github.com/amirb97/desktop-wallpaper-voting-site',
+            image: require('../../assets/voto.png')
+        },
+        {
+            title: 'Take A Hike',
+            liveLink: 'https://amirb97.github.io/take-a-hike/',
+            repoLink: 'https://github.com/amirb97/take-a-hike',
+            image: require('../../assets/take-a-hike.png')
+        },
+        {
+            title: 'Tech Blog',
+            liveLink: 'https://protected-dawn-79588.herokuapp.com/',
+            repoLink: 'https://github.com/amirb97/MVC-Tech-Blog-Challenge',
+            image: require('../../assets/tech-blog.png')
+        },
+        {
+            title: 'Weather Dashboard',
+            liveLink: 'https://amirb97.github.io/weather-dashboard-challenge/',
+            repoLink: 'https://github.com/amirb97/weather-dashboard-challenge',
+            image: require('../../assets/weather-dashboard.png')
+        },
+        {
+            title: 'Password Generator',
+            liveLink: 'https://amirb97.github.io/password-generator-challenge/',
+            repoLink: 'https://github.com/amirb97/password-generator-challenge',
+            image: require('../../assets/password-generator.png')
+        }
+    ]
+
     return (
-        <div>
-            Portfolio
+        <div className="text-center">
+            <h1 className="border-bottom border-dark p-3">Portfolio</h1>
+            <p>Just some of the projects I've worked on with links to their respective GitHub repos and live deployements.</p>
+            <div className="flex-row justify-content-around">
+                {projects.map((project) => (
+                    <div key={project.title} className='m-3 p-3 rounded shadow-lg'>
+                        <h2>{project.title}</h2>
+                        <img
+                          src={project.image}
+                          alt={project.title}
+                          className='img-thumbnail'
+                        />
+                        <div className="m-3">
+                            <a href={project.liveLink} className='text-decoration-none'>Deployed Link</a>
+                            <br></br>
+                            <a href={project.repoLink} className='text-decoration-none'>GitHub Repo Link</a>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
