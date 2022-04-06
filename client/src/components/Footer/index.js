73e8948e6e759302cea0995b33fd0dc3af3aa4cd
Era <amirb2467@gmail.com>
2022-04-06 15:07:41 -0700

    created footer component with links to github, indeed, and linkedin

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,11 +4,14 @@ function Footer() {
     return (
         <footer className="w-100 mt-auto p-4">
             <div className="container flex-row">
-                <div className="col-6 text-center">
-                    Github
+                <div className="col-4 text-center">
+                    <a href="https://github.com/amirb97" target="_blank" className="btn btn-outline-light">Github</a>
                 </div>
-                <div className="col-6 text-center">
-                    LinkedIn
+                <div className="col-4 text-center">
+                    <a href="https://www.linkedin.com/in/amir-bahmani-ab790b21b/" target="_blank" className="btn btn-outline-light">LinkedIn</a>
+                </div>
+                <div className="col-4 text-center">
+                    <a href="https://my.indeed.com/resume?hl=en&co=US&from=gnav-menu-homepage&_ga=2.138860467.1252526736.1649282605-584775759.1649282605" target="_blank" className="btn btn-outline-light">Indeed</a>
                 </div>
             </div>
         </footer>
