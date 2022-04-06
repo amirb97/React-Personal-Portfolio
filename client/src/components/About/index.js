fe37546791b591ad314c2604082272ba2bdf0bad
Era <amirb2467@gmail.com>
2022-04-06 13:30:27 -0700

    created About section

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -3,7 +3,16 @@ import React from "react";
 function About() {
     return (
         <div>
-            About
+            <h1 className="text-center border-bottom border-dark p-3">About Me</h1>
+            <div className="text-center p-2">
+                <img src={require('../../assets/profile-pic.jpg')} className='img-thumbnail rounded'></img>
+                <p>
+                    I'm a current Web Developer Student looking to learn and perfect skills that can propel myself into a position to take on any project no matter the scale. From small start-ups to large corporations I aim to be able to create applications that enable future employers to reach their goals.
+                    <br></br>
+                    <br></br>
+                    I look forward to speaking with you and assisting in any way I can.
+                </p>
+            </div>
         </div>
     );
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,9 +2,16 @@ import React from "react";
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="w-100 mt-auto p-4">
+            <div className="container flex-row">
+                <div className="col-6 text-center">
+                    Github
+                </div>
+                <div className="col-6 text-center">
+                    LinkedIn
+                </div>
+            </div>
+        </footer>
     );
 }
 
